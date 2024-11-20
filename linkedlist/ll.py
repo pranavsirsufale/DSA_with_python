f3c0576a4b5aa1154af2b1c051b335730f96e4dc
Pranav sirsufale <pranavdada334@gmail.com>
2024-11-20 10:11:12 +0530

    add linke list

--- linkedlist/ll.py
+++ linkedlist/ll.py
@@ -9,3 +9,33 @@ class Linedlist:
         self.head = None
         # no of nodes in the ll
         self.n = 0
+
+    def __len__(self):
+        return self.n
+    
+
+    # ******
+    def insert_head(self,value):
+        # new node 
+        new_node = Node(value)
+        # Create connection
+        new_node.next = self.head
+        # reassign head
+        self.head = new_node
+        #increment 
+        self.n += 1
+
+    def __str__(self):
+        curr = self.head
+        result = ''
+        while curr != None:
+            result = result + str(curr.data) + '->'
+            curr = curr.next
+        return result[:-2]
+    
+
+l = Linedlist()
+print(l.insert_head(10))
+print(l.insert_head(20))
+print(l.insert_head(30))
+print(l.head)
