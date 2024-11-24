5c4ffa105487b386a9c7a4f25f711cd54c7f64c8
Pranav sirsufale <pranavdada334@gmail.com>
2024-11-24 08:22:14 +0530

    add array

---
+++ array/insertion_sort.py
@@ -0,0 +1,23 @@
+'''
+algo
+1 ) we start with second element of the array as fist
+elemn in the array is assumed to be sorted.
+
+2) Compare second element with th efirst eleme and check
+if the second element is smaller than swap them
+
+3) move to the third element and compare it wit the 
+second element, then the first element and swap as necessary to put it in the 
+correct position among the first there elements.
+
+
+4) continue this process, computing each element with the ones befroe it and swapping
+as needed to place it in the correct
+position among th esorted elem 
+
+5) Repeat until the entire array is sorted
+
+
+
+
+'''
