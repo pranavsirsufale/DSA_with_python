2db66b272420e2bc59019a91a4a648e1a1ea2599
Pranav sirsufale <pranavdada334@gmail.com>
2024-11-29 22:49:02 +0530

    add bfs code

--- array/BFS.py
+++ array/BFS.py
@@ -1,17 +1,36 @@
+from collections import deque
+
 def bfs(graph,start):
-    visited = set()   # set keep track of visited notes
-    queue = []      # que to take reference
-
-    queue.append(start)
-    visited.add(start)
-
+    visited = set()         # To track visited nodes
+    queue = deque([start])    # Initial a queue with the start
+    visited.add(start)      # Mark the start node as visisted
 
     while queue:
-        node = queue.pop(0)
-        print(node)
+        node = queue.popleft()  # Dequeue a node from the fton
+        print(node,end=" ")     # Proess the node ( e.g, print)
 
-        # Explore neighbors
-        for neighbor in grap[node]:
+
+        # Iterate through the neigbhors of the current node
+        for neighbor in graph[node]:
             if neighbor not in visited:
-                queue.append(neighbor)
-                visited.add(neighbor)
+                queue.append(neighbor) # Enqueue unvisited neighours
+                visited.add(neighbor)   # Mark them as visited
+
+
+# Example grpah represented as an adjecency list
+graph = {
+    'A':['B','C'],
+    'B': ['A','D','E'],
+    'C' : ['A','F'],
+    'D': ['B'],
+    'E' : ['B'],
+    'F':['C']
+}
+
+# Run BFS starting from node 'A"
+print('BFS Traversal : ')
+
+bfs(graph,'A')
+
+
+
