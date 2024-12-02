57497101677772b5b1ff2e3a0d94c1a23816b7e0
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-02 08:18:54 +0530

    add graph

--- dv/dataframe.py
+++ dv/dataframe.py
@@ -85,7 +85,6 @@ xpoints = np.array([0,6])
 ypoints = np.array([0,250])
 plt.plot(xpoints,ypoints)
 plt.show()
-'''
 
 
 
@@ -93,3 +92,16 @@ plt.show()
 ypoints = np.array([3,8,1,10])
 plt.plot(ypoints,marker='o')
 plt.show()
+
+
+'''
+
+
+
+
+
+
+
+
+
+
