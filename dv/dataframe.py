faf5fc4588f6926ca8ea3bf7904cded6e6263ce0
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-02 08:08:32 +0530

    add data visualization

--- dv/dataframe.py
+++ dv/dataframe.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 '''
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -11,19 +13,53 @@ plt.xlabel("Student Name")
 plt.ylabel('Student Mark')
 plt.title("Student Makrs")
 # plt.show()
-'''
 
 import matplotlib.pyplot as plt
+import numpy as np
 x = [1,2,3,4,5,6,7,8,9,10]
 y = [10,20,30,40,50,60,70,80,90,100]
 plt.scatter(x,y)
 plt.xlabel('x-axis')
 plt.ylabel('y-label')
 plt.title('scatter graph')
+plt.show()
+
+
+
+
+
+import matplotlib.pyplot as plt
+stud_name = ['ram','sham','rahul','rajesh','sahil']
+stud_mark = [55,66,58,32,65]
+
+plt.plot(stud_name,stud_mark)
+plt.xlabel('Student Name')
+plt.ylabel("Student marks ")
+plt.title('Line Graph')
+plt.show()
 
 
 
 
 
 
+x = [1,2,3,4,5,6,7,8,9,10]
+y= [10,20,30,40,50,60,70,80,90,100]
+plt.scatter(x,y)
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+plt.title('Scatter graph')
+plt.show()
+# scatter plot
+
+
+
+x = np.array([5,7,8,7,2,7,2,9,4,11,12,9,6])
+y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+plt.xlabel("X - asxi")
+plt.ylabel('Y- lables')
+plt.scatter(x,y)
+plt.show()
+
+'''
 
