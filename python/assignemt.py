5ba52a765cdf54a0da007e6322d08787c79388bf
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-04 19:35:04 +0530

    assignemt python

--- python/assignemt.py
+++ python/assignemt.py
@@ -1,12 +1,36 @@
 import math
+
+
 # Q 1) Write a python program to accept the radius from the user and print area of the circle?
 
 # area of circle Pi * r ^2
 
-
+'''
 def area_of_circle(r):
     return ((math.pi) * (r**2))
 
 radiusofcircle = float(input('enter the area of radius : '))
 
 print(area_of_circle(radiusofcircle))
+
+'''
+
+
+#Write a python program to calculate the area of a square
+# formulat side * side
+
+def area_of_square(side):
+    return (side*side)
+
+
+'''
+side = float(input('enter length of the side : '))
+
+print(area_of_square(side))
+
+'''
+
+#Write a python program to calculate the area of rectangle
+# area of rectangle = side * length
+
+
