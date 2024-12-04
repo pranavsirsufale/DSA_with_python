389161d10674553a7979828d33e7b3abd7929504
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-04 22:06:25 +0530

    assignemt python

--- python/assignemt.py
+++ python/assignemt.py
@@ -110,6 +110,32 @@ digit = 1234569 # 21
 print(calculate_sum(digit))
 '''
 
+#Write a python program for reverse a given number using a while loop.
+
+def reverse_number(n):
+    # n = [i for i in str(n)]
+    # second_pointer = len(n)-1
+    # print(n)
+    # for i in range(int(len(n)/2)):
+    #     n[i],n[second_pointer] = n[second_pointer],n[i]
+    #     second_pointer -= 1
+    # return n
+    reversed_number = 0
+
+    while n > 0 :
+        last_digit = n % 10
+        reversed_number = (reversed_number * 10 ) + last_digit
+        n //= 10
+    return reversed_number 
+
+
+
+number = 13132
+
+rev_num = reverse_number(number)
+print(rev_num)
+
+
 
 
 
