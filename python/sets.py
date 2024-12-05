d7ace2c0379ba1b6ff2389e24fc572c092827815
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-05 11:37:46 +0530

    add assignemtn

--- python/sets.py
+++ python/sets.py
@@ -56,3 +56,20 @@ print("Set after clearing all elements:", my_set)
 '''
 
 
+#Write a Python program to calculate the distance between the points (x1, y1) and (x2, y2).
+
+'''
+import math
+def calculate_distance(x1, y1, x2, y2):
+    # Calculate the distance using the distance formula
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return distance
+
+# Example usage
+x1, y1 = 1, 2
+x2, y2 = 4, 6
+
+distance = calculate_distance(x1, y1, x2, y2)
+print(f"Distance between ({x1}, {y1}) and ({x2}, {y2}) is: {distance}")
+'''
+
