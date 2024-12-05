7679a3892c38a4cc698c606be54968f80c6eefc1
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-05 11:09:52 +0530

    add assignemtn

--- python/sets.py
+++ python/sets.py
@@ -31,5 +31,28 @@ print("Set after adding 5, 6, 7:", my_set)
 '''
 
 
+# Write a Python program to delete member(s) to a set.
+
+'''
+# Creating a set
+my_set = {1, 2, 3, 4, 5}
+
+# Using remove() to delete an element
+my_set.remove(3)
+print("Set after removing 3:", my_set)
+
+# Using discard() to delete an element (no error if element is missing)
+my_set.discard(2)
+print("Set after discarding 2:", my_set)
+
+# Using pop() to remove a random element
+removed_element = my_set.pop()
+print(f"Set after popping an element: {my_set}, Removed element: {removed_element}")
+
+# Using clear() to remove all elements
+my_set.clear()
+print("Set after clearing all elements:", my_set)
+
+'''
 
 
