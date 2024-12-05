6673b2f329f15160d942884415900f41c63c422c
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-05 11:05:27 +0530

    add assignemtn

---
+++ python/sets.py
@@ -0,0 +1,16 @@
+# Write a python program to create a set.
+
+'''
+# Creating a set using curly braces
+my_set = {1, 2, 3, 4, 5}
+
+# Creating a set using the set() constructor
+another_set = set([6, 7, 8, 9, 10])
+
+# Printing the sets
+print("Set created using curly braces:", my_set)
+print("Set created using set() constructor:", another_set)
+
+'''
+
+
