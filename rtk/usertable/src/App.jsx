6eb3f5225e7eb5cc31f08a6467f812c310004e02
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-11 13:35:09 +0530

    add rtk

--- rtk/usertable/src/App.jsx
+++ rtk/usertable/src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 import UserDetails from './components/UserDetails'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+
     <>
       <Navbar/>
 
