4fd5aed7f1c9f98fa422c089740d90e3d0bccd73
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-14 22:20:04 +0530

    add a problem

--- jsproblems/jsproblem.js
+++ jsproblems/jsproblem.js
@@ -1,6 +1,5 @@
+//  Q1) 
 
-
-// Q1) 
 //? written by me 
 /* 
 const fetchData = async () => {
@@ -53,7 +52,74 @@ function fetchData() {
 */
 
 
+//! Q2>>> Q.2) Write a function parseJSON() that:
+//?>>• Takes a JSON string as input.
+//?>> • Returns the parsed JavaScript object if the JSON string is valid.
+//?>>• If the JSON string is invalid (i.e., it cannot be parsed), catch the error and return the message "Invalid JSON".
 
+
+const valid_json = JSON.stringify({
+    "employees": [
+      {
+        "id": 1,
+        "name": "John Doe",
+        "age": 29,
+        "department": "Engineering",
+        "skills": ["JavaScript", "React", "Node.js"],
+        "isFullTime": true
+      },
+      {
+        "id": 2,
+        "name": "Jane Smith",
+        "age": 34,
+        "department": "Marketing",
+        "skills": ["SEO", "Content Writing", "Social Media"],
+        "isFullTime": false
+      },
+      {
+        "id": 3,
+        "name": "Mike Johnson",
+        "age": 41,
+        "department": "Human Resources",
+        "skills": ["Recruitment", "Employee Relations", "Training"],
+        "isFullTime": true
+      }
+    ]
+  })
+
+const invalid_json = {
+    "employees": [
+      {
+        id: 1,              // Missing quotes around key
+        "name": "John Doe",
+        "age": 29,
+        "department": "Engineering",
+        "skills": ["JavaScript", "React", "Node.js"],
+        "isFullTime": true,
+      },                    // Trailing comma here
+      {
+        "id": 2,
+        "name": "Jane Smith",
+        "age": "34",        // Age should be a number, not a string
+        "department": null,
+        "skills": "SEO",    // Skills should be an array, not a string
+        "isFullTime": false
+      }
+    ]
+  }
+  
+const json_validator_fun = (jsonData) => {
+    try {
+        return JSON.parse(jsonData)
+    } catch (error) {
+        // console.log(error.messege || error);
+        return 'Invalid Json'
+        
+    }
+}
+console.log( " VALID JSON DATA : " ,json_validator_fun(valid_json))
+
+console.log("INVALID JSON DATA : ", json_validator_fun(invalid_json))
 
 
 
