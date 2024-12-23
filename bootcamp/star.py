0441ef01afdd6251ab7b7f1b3569b2ed80774591
Pranav sirsufale <pranavdada334@gmail.com>
2024-12-23 21:19:30 +0530

    add a start pattern problem

--- bootcamp/star.py
+++ bootcamp/star.py
@@ -7,3 +7,15 @@ till = 5
 print(print_star(till))
 
 
+def alagram(text1,text2):
+    if len(text1) != len(text2):
+        return 'strings are not having same length'
+    if sorted(text1) != sorted(text2):
+        return 'not sharing same characters '
+    else:
+        return f" The both strings '{text1}' and '{text2}' are sharing the same characters"
+    
+tex1 = 'something'
+tex2 = 'thingsome'
+
+print(alagram(tex1,tex2))
