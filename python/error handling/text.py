4b71c1bbb72349fada65c70d96e65749b5c1c14e
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-07 19:42:39 +0530

    add django backend code

--- python/error handling/text.py
+++ python/error handling/text.py
@@ -3,7 +3,7 @@ import json
 
 def list_all_videos(videos):
     for index,video in enumerate(videos,start=1):
-        print(f'{index}')
+        print(f'{index} and video is {video}')
 
 def add_video(videos):
     name = input('Etner video name : ')
@@ -25,6 +25,7 @@ def load_data():
         with open('youtuve.txt','r') as file:
             return json.load(file)
     except FileNotFoundError:
+        print('an empty file is returned')
         return []
 
 def save_data_helper(videos):
@@ -40,7 +41,7 @@ def main():
         print('3. Update a youtube video details')
         print('4. Delete a youtube video')
         print('5. Exit the app')
-        choice = input('Enter your choide')
+        choice = input('Enter your choide : ')
 
         match choice:
             case '1':
