53760f3ed793497ddc365b1fec59baacd5cc50f6
Pranav sirsufale <pranavdada334@gmail.com>
2025-02-07 14:54:35 +0530

    class and objects ooops programming

---
+++ python/decorators/deco.py
@@ -0,0 +1,37 @@
+# learn about decorators 
+#? Timiing function Execution
+#//! Write a decorator that measures the time a function takes to execute 
+
+import time 
+
+
+def timer(func):
+    def wrapper(*args,**kwars):
+        start = time.time()
+        result = func(*args, **kwars)
+        end = time.time()
+
+        print(f'{func.__name__} ran in {end-start} time')
+        return result
+
+    return wrapper
+
+
+@timer
+def example_function(n):
+    time.sleep(n)
+
+
+print(example_function(2))
+
+
+
+
+
+
+
+
+
+
+
+
