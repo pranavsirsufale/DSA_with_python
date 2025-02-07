7a2478b254f6d641e09ff14eaa8112562ef013dd
Pranav sirsufale <pranavdada334@gmail.com>
2025-02-07 11:47:54 +0530

    add python sql alchemy

--- python/alchemy/alchemm.py
+++ python/alchemy/alchemm.py
@@ -2,15 +2,19 @@ from sqlalchemy import *
 
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
 
-with engine.begin() as conn:
+with engine.connect() as conn:
     conn.execute(
         text('create table some_table(x int, y int)')
      )
     
-    resutl = conn.execute(
+    conn.execute(
         text('insert into some_table(x, y) values(:x, :y)'),
-        [{'x':10, "y":20},{'x':30,'y':40}]
+        [{'x':50, "y":60},{'x':70,'y':80}]
     )
+
+    result = conn.execute(text("SELECT x, y FROM some_table"))
+    for row in result:
+        print(f"x: {row.x}  y: {row.y}")
 
     print(Result)
     
