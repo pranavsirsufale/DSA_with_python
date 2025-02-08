f0d2b98535d090bc0807f3c5c9026fee86b0c941
Pranav sirsufale <pranavdada334@gmail.com>
2025-02-08 16:36:59 +0530

    decorators

--- python/decorators/decorators.py
+++ python/decorators/decorators.py
@@ -21,12 +21,10 @@ def greet(name, greet):
 greet('pranav' ,'hellow')
 '''
 
-
 #??? chache Return Value
 #! Implement a decorator that caches the return values of a function, so that when it's called with the same arguments , the cached value is returned instead of re-executing the function
 
 import time
-
 
 def cache(func):
     cache_value = {}
@@ -39,24 +37,10 @@ def cache(func):
         return result
     return wrapper
 
-
-
 @cache
 def long_running_function(a,b):
     time.sleep(4)
     return a + b 
 
-
 print(long_running_function(2,3))
 print(long_running_function(2,3))
-
-
-
-
-
-
-
-
-
-
-
