cce2c195153bd73c986f73e8f9df9537a15699ec
Pranav sirsufale <pranavdada334@gmail.com>
2025-02-08 16:25:19 +0530

    sql alchemy

--- python/decorators/decorators.py
+++ python/decorators/decorators.py
@@ -4,22 +4,22 @@
 
 #//! Create a decorator to print the function name and the values of its arguents every time the function is called.
 
+'''
+def debug(func):
+    def wrapper(*args,**kwargs):
+        args_value = ', '.join(str(arg) for arg in args)
+        kwargs_value = ', '.join(f'{k}={v}' for k,v in kwargs.items())
+        print(f'calling : {func.__name__} with args {args_value} and kwargs {kwargs_value}')
+        return func(*args,**kwargs)
+    return wrapper
 
-# def debug(func):
-#     def wrapper(*args,**kwargs):
-#         args_value = ', '.join(str(arg) for arg in args)
-#         kwargs_value = ', '.join(f'{k}={v}' for k,v in kwargs.items())
-#         print(f'calling : {func.__name__} with args {args_value} and kwargs {kwargs_value}')
-#         return func(*args,**kwargs)
-#     return wrapper
-
-# @debug
-# def greet(name, greet):
-#     print(f'{greet} , {name}')
+@debug
+def greet(name, greet):
+    print(f'{greet} , {name}')
 
 
-# greet('pranav' ,'hellow')
-
+greet('pranav' ,'hellow')
+'''
 
 
 
