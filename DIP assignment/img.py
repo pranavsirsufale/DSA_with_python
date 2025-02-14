9c2c0b15847bc046dbd151ba6d88cdcc305105c3
Pranav sirsufale <pranavdada334@gmail.com>
2025-02-14 09:52:55 +0530

    add DIP program

--- DIP assignment/img.py
+++ DIP assignment/img.py
@@ -162,7 +162,22 @@ cv2.waitKey()
 '''
 
 
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+img = cv2.imread('cam.jpg')
+img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+gradient_laplacian = cv2.Laplacian(img, -1 , )
+fig , axis = plt.subplots(2,3,figsize = (10 , 5 ))
+axis[0,0].imshow(img)
+axis[0,0].set_title('original image')
+axis[0,0].axis('off')
 
+axis[1,1].imshow(gradient_laplacian)
+axis[1,1].set_title('laplacian image is')
+axis[1,1].axis('off')
+plt.show()
+cv2.waitKey()
 
 
 
