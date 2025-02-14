84491fd758754f1987b11902b0b9a2206264eb80
Pranav sirsufale <pranavdada334@gmail.com>
2025-02-14 10:19:22 +0530

    add DIP program

--- DIP assignment/img.py
+++ DIP assignment/img.py
@@ -196,7 +196,7 @@ plot_histogram(dark,"dark")
 plt.title("dark image histogram")
 plt.tight_layout()
 plt.show()
-'''
+
 import cv2
 image = cv2.imread('skel.jpg', 0)
 # Load as grayscale (0)
@@ -206,3 +206,4 @@ cv2.imshow('Original Image', image)
 cv2.imshow('Thresholded Image', thresholded_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+'''
