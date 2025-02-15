4ea3c7a75795b9e0acc05a7ed4298f80349c72fd
Pranav sirsufale <pranavdada334@gmail.com>
2025-02-15 09:07:57 +0530

    add DSA problem in python

--- python/graph/recursion.py
+++ python/graph/recursion.py
@@ -1,6 +1,7 @@
 ## python code to implement fibonnacci series
 # function for fibonaccie
 def fib(n):
+    print(n)
     # stop condition 
     if(n == 0 ):
         return 0
@@ -8,4 +9,15 @@ def fib(n):
     if ( n== 1 or n == 2 ):
         return 1
     # recursive function
-    
+    else : 
+        return ( fib(n-1 ) + fib(n-2))
+
+# driver code
+if __name__ == "__main__":
+    # initial variable
+    n = 5 
+    print('fibonaccie series of number is : ', end = " ")
+
+    # for loop to print the fibonaccie series 
+    for i in range(0,n):
+        print(fib(i),end=' ')
