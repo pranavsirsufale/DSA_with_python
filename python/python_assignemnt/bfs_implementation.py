01b8d98d9c3b04336024626988980c8910f8b006
Pranav sirsufale <pranavdada334@gmail.com>
2025-02-18 07:48:43 +0530

    add heap problem

--- python/python_assignemnt/bfs_implementation.py
+++ python/python_assignemnt/bfs_implementation.py
@@ -5,19 +5,30 @@
 
 ##?  3: While the queue is not empty, dequeue a node, print it. 
 
-
 from collections import deque
-
 def bfs(graph,start):
     visited = []
-
     queue = deque([start])
-
     while queue:
         node = queue.popleft()
-
         if node not in visited:
             print(node , end = ' ')
             visited.append(node)
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
 
+
+# example usage 
+graph = {
+    'a' : ['b', 'c'],
+    'b' : ['a','d','e'],
+    'c' : [ 'a' , 'f'],
+    'd' : ['b'],
+    'e': ['b','f'],
+    'f' : [ 'c','e']
+}
+
+
+bfs(graph , 'a')
 
