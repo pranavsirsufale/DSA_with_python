a256d47de088ffbb32286a00a35996f4b12cd8e1
Pranav sirsufale <pranavdada334@gmail.com>
2025-02-18 20:21:05 +0530

    add heap problem

---
+++ python/python_assignemnt/heap_sort.py
@@ -0,0 +1,12 @@
+
+'''
+###?? 1]Build a max heap.
+
+###?? 2]Swap the root (largest element) with the last element.
+
+###?? 3]Reduce the heap size and heapify the tree again.
+
+###?? 4]Repeat until the heap size is reduced to 1.
+'''
+
+
