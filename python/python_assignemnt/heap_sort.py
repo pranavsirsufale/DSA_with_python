6b4529d2725fbbc77a0dde7f90de8f163c5c1231
Pranav sirsufale <pranavdada334@gmail.com>
2025-02-18 20:26:43 +0530

    add heap problem

--- python/python_assignemnt/heap_sort.py
+++ python/python_assignemnt/heap_sort.py
@@ -22,3 +22,14 @@ def heapify(arr,n,i):
     if largest != i:
         arr[i] , arr[largest] = arr[largest] , arr[i]
         heapify(arr,n,largest)
+
+
+def heap_sort(arr):
+    n = len(arr)
+    for i in range(n // 2 - 1 , -1 , -1 ):
+        heapify(arr,n,i)
+
+    for i in range(n-1,0,-1):
+        arr[i] , arr[0] = arr[0] , arr[i]
+        heapify(arr,i,0)
+        
