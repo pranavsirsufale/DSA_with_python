73d49d9ae1ae97d16de8ed672f6a1c24dfb0ba15
Pranav sirsufale <pranavdada334@gmail.com>
2025-02-19 00:08:45 +0530

    add heap problem

--- python/python_assignemnt/selection_sort.py
+++ python/python_assignemnt/selection_sort.py
@@ -8,16 +8,3 @@
 #? 3 ] Move the boundary between sorted and unsorted parts.
 '''
 
-
-def selection_sort(arr):
-    for i in range(len(arr)):
-        min_idx = i 
-        for j in range(i+1 , len(arr)):
-            if arr[j] < arr[min_idx]:
-                min_idx = j
-        arr[i] , arr[min_idx] = arr[min_idx],arr[i]
-
-# Example usage
-arr = [29, 10, 14, 37, 13]
-selection_sort(arr)
-print(arr)
