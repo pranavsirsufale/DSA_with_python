7d5b28a2280d86b86bfff46e115f995e68e8892e
Pranav sirsufale <pranavdada334@gmail.com>
2025-02-19 11:03:33 +0530

    add heap problem

--- ML/toy_project.py
+++ ML/toy_project.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
 
 df = pd.read_csv('ML/placement-dataset.csv')
 
@@ -48,3 +50,15 @@ y = df.iloc[:,-1]
 X_train , X_test , y_train, y_test = train_test_split(X, y, test_size= 0.1, )
 
 
+scaler = StandardScaler() 
+
+X_train = scaler.fit_transform(X_train)
+
+X_test = scaler.transform(X_test)
+
+
+clf = LogisticRegression() 
+
+#### Model training
+
+clf.fit(X_train,y_train)
