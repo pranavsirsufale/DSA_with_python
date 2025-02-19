40e5f4ef30787914a66ad6b21ef3004087fe4cdd
Pranav sirsufale <pranavdada334@gmail.com>
2025-02-19 10:43:35 +0530

    add heap problem

--- ML/toy_project.py
+++ ML/toy_project.py
@@ -6,10 +6,20 @@ df = pd.read_csv('ML/placement-dataset.csv')
 print(df.head())
 
 df = df.iloc[:,1:]
-print('_____________MAXIMUM ------------------------------')
-print(df.max())
-print('------------------- MININUM  ------------------------------')
-print(df.min())
-print('----------------------- RANGE------------------------------')
-print((df.max()) - (df.min()))
+# print('_____________MAXIMUM ------------------------------')
+# print(df.max())
+# print('------------------- MININUM  ------------------------------')
+# print(df.min())
+# print('----------------------- RANGE------------------------------')
+# print((df.max()) - (df.min()))
 # print(df)
+
+
+## STEPS 
+###? 0.  PREPROCESSING - EDA + FEATURE SELECTION
+###? 1 . eXTRACT INPUT AND OUT COLS
+###? 2. SCALE THE VALUES
+###? 3. TRAIN TEST SPLIT
+###? 4. TRAIN THE MODEL
+###? 5. EVALUATE THE MODEL/MODEL SELECTION
+###? 6 . DEPLOY THE MODEL 
