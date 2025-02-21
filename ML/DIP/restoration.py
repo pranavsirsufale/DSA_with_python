13ac4f4fd86bddb33a648f3f110f4a629be75d41
Pranav sirsufale <pranavdada334@gmail.com>
2025-02-21 21:19:15 +0530

    ab ML project

---
+++ ML/DIP/restoration.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load a sample medical image ( grayscale )
+# Replace *medical_image with the path no your image
+image = cv2.imread('')
+
+
