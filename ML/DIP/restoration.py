dde2a4ba2367cb6eeee7d5b3fbb7fadb78169d30
Pranav sirsufale <pranavdada334@gmail.com>
2025-02-21 21:37:04 +0530

    ab ML project

--- ML/DIP/restoration.py
+++ ML/DIP/restoration.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # Load a sample medical image ( grayscale )
 # Replace *medical_image with the path no your image
-image = cv2.imread('/DIP/medical.jpg',cv2.IMREAD_GRAYSCALE)
+image = cv2.imread('F:/DSA/practical/ML/DIP/medical.jpg',cv2.IMREAD_GRAYSCALE)
 
 
 if image is None:
@@ -22,8 +22,26 @@ denoised_image = cv2.GaussianBlur(noisy_image,(5,5),0)
 # Plot the original noisy, and restored images
 plt.figure(figsize=(12,8))
 
+plt.subplot(1,3,1)
+plt.title('Original Image')
+plt.imshow(image,cmap='gray')
 
+
+
+plt.subplot(1,3,2)
+plt.title('Noisy Image')
+plt.imshow(noisy_image,cmap='gray')
+plt.axis('off')
+
+plt.subplot(1,3,3)
+plt.title('Restored Image ( Gaussian Filter )')
+plt.imshow(denoised_image,cmap='gray')
+plt.axis('off')
+
+plt.tight_layout()
 plt.show()
+
+
 
 
 
