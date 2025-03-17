5dd98b510e6d6a9554e5fcebed75e255599f67af
Pranav sirsufale <pranavdada334@gmail.com>
2025-03-18 00:32:57 +0530

    add an algorithm

--- graph/dikstra.py
+++ graph/dikstra.py
@@ -6,4 +6,20 @@ def dijkstra(graph,start):
     distances[start] = 0 
     heapq.heappush(pq, (0 , start))
 
-    
+    while pq:
+        current_distance , current_node = heapq.heappop(pq)
+
+        if current_distance > distances[current_node]:
+            continue
+
+        for neighbor, weight in graph[current_node]:
+            distance = current_distance + weight
+
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(pq,(distance,neighbor))
+
+    return distances
+
+
